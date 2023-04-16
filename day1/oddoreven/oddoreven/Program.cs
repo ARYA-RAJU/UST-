d80c0f554ede0e57be6b63501cc2235b3c95d9d5
ARYA-RAJU <129927863+ARYA-RAJU@users.noreply.github.com>
2023-04-16 22:08:15 +0530

    Add files via upload

---
+++ day1/oddoreven/oddoreven/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddoreven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter the number :");
+            x=int.Parse(Console.ReadLine());
+            if(x%2==0 )
+            {
+                Console.WriteLine("number is even");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+                Console.ReadKey();
+            }
+        }
+    }
+}

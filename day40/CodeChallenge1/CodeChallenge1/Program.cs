d3d2588f4b2cd02f08c6dfb2b9c036cf5c94c37a
ARYA-RAJU <129927863+ARYA-RAJU@users.noreply.github.com>
2023-05-29 09:23:34 +0530

    Add files via upload

---
+++ day40/CodeChallenge1/CodeChallenge1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge1
+{
+    
+    class Inner
+    {
+
+        public class Private
+        {
+            public bool Power(int n)
+            {
+                if(n==0)
+                
+                    return false;
+                
+                while(n!=1)
+                {
+                    if (n % 2 != 0)
+                    {
+                        return false;
+                    }
+                     n = n / 2;
+                    
+                    
+                }
+                return true;
+            }
+        }
+        class Solution
+        {
+            static void Main(string[] args)
+            {
+                Inner.Private innerprivate = new Inner.Private();
+                Console.WriteLine("Enter the number:");
+                int n = int.Parse(Console.ReadLine());
+                if (innerprivate.Power(n))
+                {
+                    Console.WriteLine(n + " is power of 2");
+                }
+                else
+                {
+                    Console.WriteLine(n + " is not power of 2");
+                }
+            }
+        }
+    }
+}
---
+++ day40/CodeChallenge2/CodeChallenge2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeChallenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of sente1nces:");
+            int n=int.Parse(Console.ReadLine());
+            string[] str = new string[n];
+            for(int i=0;i<n;i++)
+            {
+                str[i] = Console.ReadLine();
+            }
+            foreach(string str1 in str)
+            {
+                string result = RemoveDuplicateWords(str1);
+                Console.WriteLine(result);
+            }
+        }
+        static string RemoveDuplicateWords(string sentence)
+        {
+            string pattern = @"\b(\w+)\b\s+(?=.*\b\1\b)";
+            string rep = "";
+            Regex regex = new Regex(pattern);
+            string result=regex.Replace(sentence, rep);
+            return result;
+        }
+    }
+}

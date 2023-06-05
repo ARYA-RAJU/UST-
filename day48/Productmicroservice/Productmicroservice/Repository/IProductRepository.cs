047a03286804a57dd77fc15bc20f6c035d765757
ARYA-RAJU <129927863+ARYA-RAJU@users.noreply.github.com>
2023-06-05 09:31:12 +0530

    Add files via upload

---
+++ day48/Productmicroservice/Productmicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Productmicroservice.Model;
+using System.Collections.Generic;
+namespace Productmicroservice.Repository
+{
+   public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductByID(int ProductId);
+        void InsertProduct(Product Product);
+        void DeleteProduct(int ProductId);
+        void updateProduct(Product Product);
+        void Save();
+    }
+}
